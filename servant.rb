33f486c138d0e9f4b5690f757d11cb4453396151
cutem-0104 <shuhei.sugaya@gmail.com>
2018-08-10 21:58:49 +0900

    fix get servant data.

--- servant.rb
+++ servant.rb
@@ -2,10 +2,11 @@ require 'open-uri'
 require 'nokogiri'
 
 url = 'https://grand_order.wicurio.com/index.php?'
-param = '%E3%83%9E%E3%82%B7%E3%83%A5%E3%83%BB%E3%82%AD%E3%83%AA%E3%82%A8%E3%83%A9%E3%82%A4%E3%83%88'
+param1 = '%E3%83%9E%E3%82%B7%E3%83%A5%E3%83%BB%E3%82%AD%E3%83%AA%E3%82%A8%E3%83%A9%E3%82%A4%E3%83%88'
+param2 = '%E3%82%A2%E3%83%AB%E3%83%88%E3%83%AA%E3%82%A2%E3%83%BB%E3%83%9A%E3%83%B3%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3'
 
 charset = nil
-html = open(url + param) do |f|
+html = open(url + param2) do |f|
   charset = f.charset
   f.read
 end
@@ -31,12 +32,23 @@ txt.each{|ele|
   p ele.inner_text
 }
 
-p "-------------------------"
+p "-----------------------"
 tr.each{|ele|
+  p "---------------------"
   key = ele.css("th").inner_text
   value = ele.css("td").inner_text
   p key + " : " + value
-  if (ele.css("td").kind_of?(Array)) then
-    p "true"
-  end
+
+  keys = ele.css("th")
+  values = ele.css("td")
+
+  p
+
+  keys.each{|key|
+    p "key =  " + key
+  }
+
+  values.each{|value|
+    p "value = " + value
+  }
 }
