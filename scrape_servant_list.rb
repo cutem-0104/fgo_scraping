24c5ddf60d0fe9bb089e0e43bffc26d97fa78cc7
cutem-0104 <shuhei.sugaya@gmail.com>
2018-08-13 01:21:12 +0900

    add servant list acquisition function

--- scrape_servant_list.rb
+++ scrape_servant_list.rb
@@ -4,6 +4,7 @@
 require 'open-uri'
 # Nokogiriライブラリの読み込み
 require 'nokogiri'
+require 'mechanize'
 
 # スクレイピング先のURL
 url = 'https://grand_order.wicurio.com/index.php?%E3%82%B5%E3%83%BC%E3%83%B4%E3%82%A1%E3%83%B3%E3%83%88%E4%B8%80%E8%A6%A7'
@@ -22,11 +23,25 @@ p doc.title
 
 table = doc.css(".style_table > tbody > tr")
 
-p "servant"
+
+# サーヴァントの詳細URLを取得し、リスト化する
+url_list = Array.new
 table.each{|col|
   no = col.css("th")[0]
   name = col.css("td")[2]
   if (!name.nil?) then
-    p name.inner_text
+    servant = name.css('a')
+    if (servant.inner_text == "") then
+      next
+    end
+    p servant.inner_text
+    p servant[0][:href]
+    url_list.push(servant[0][:href])
   end
+}
+
+url_list.each{|url|
+  agent = Mechanize.new
+  page = agent.get(url)
+  p page.search('title')
 }
