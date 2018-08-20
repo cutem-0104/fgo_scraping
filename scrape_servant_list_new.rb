d443d4a3dc2d0cfe060b8ae2387792faf7a7aac4
cutem-0104 <shuhei.sugaya@gmail.com>
2018-08-21 01:07:53 +0900

    add status

--- scrape_servant_list_new.rb
+++ scrape_servant_list_new.rb
@@ -41,6 +41,7 @@ table.each{|col|
 }
 
 # 各サーヴァントのページを取得する
+p url_list.length
 url_list.each{|url|
   # agent = Mechanize.new
   # page = agent.get(url)
@@ -48,7 +49,7 @@ url_list.each{|url|
 }
 
 # サーヴァントの詳細情報を取得する
-url = url_list[195]
+url = url_list[220]
 charset = nil
 html = open(url) do |f|
   charset = f.charset
@@ -66,15 +67,60 @@ body = doc.css("#body > div > table")[0]
 tr = body.css('tr')
 
 # レア度、No
-rera = tr[0].css('th')[0].inner_text
-p rera
+rare = tr[0].css('th')[0].inner_text
+p rare
 no = tr[0].css('th')[1].inner_text
 p no
 
+# クラス、属性
+clazz = tr[1].css('td')[1].inner_text
+p clazz
+attri = tr[1].css('td')[2].inner_text
+p attri
+
+# 真名
+name = tr[2].css('td')[0].inner_text
+p name
+
+# 時代
+age = tr[3].css('td')[0].inner_text
+p age
+
+# 地域
+region = tr[4].css('td')[0].inner_text
+p region
+
+# HP/ATK
+tr[6..7].each{|score|
+  key = score.css('th').inner_text
+  value = score.css('td').inner_text
+  p key + ' : ' + value
+}
+
+# 能力値
+tr[8..10].each{|status|
+  key = status.css('th')[0].inner_text
+  value = status.css('td')[0].inner_text
+  p key + ' : ' + value
+  key = status.css('th')[1].inner_text
+  value = status.css('td')[1].inner_text
+  p key + ' : ' + value
+}
+
+# コスト
+cost = tr[11].css('td')[0].inner_text
+p cost
+
+# 保有カード
+tr[12].css('td').each{|card|
+  p card.inner_text
+}
+
 # スキル
 skill_list = Array.new
+class_skill_index = 0
 flg = false
-tr.each{|ele|
+tr.each_with_index{|ele, i|
   if (flg) then
     skill_list.push(ele)
   end
@@ -87,6 +133,7 @@ tr.each{|ele|
   end
   if (flg == true && word == 'クラススキル') then
     skill_list.pop()
+    class_skill_index = i + 1
     break
   end
 }
@@ -101,3 +148,24 @@ skill_list.each{|ele|
     end
   }
 }
+
+# クラススキル
+nobel_index = 0
+tr[class_skill_index..tr.length].each_with_index{|ele, i|
+  word = ''
+  if (!ele.css('th').empty?) then
+    word = ele.css('th')[0].inner_text
+  end
+  if (word == '宝具名') then
+    nobel_index = i + 1
+    break
+  end
+  if (ele.css('td').length == 1) then
+    p ' : ' + ele.css('td')[0].inner_text
+    next
+  end
+  p ele.css('td')[0].inner_text + ' : ' + ele.css('td')[1].inner_text
+}
+
+# 宝具
+p tr[nobel_index]
